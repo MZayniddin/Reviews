ce53d1811156f580f49cbb119b98c7ff1780deb8
M.Zayniddin <clon3.secur1ty@gmail.com>
2023-09-24 18:01:53 +0500

    changed api's baseUrl

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // https://localhost:3000
 // http://reviews-site.onrender.com
-const API = axios.create({ baseURL: "http://localhost:3000" });
+const API = axios.create({ baseURL: "http://reviews-site.onrender.com" });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("profile")) {
